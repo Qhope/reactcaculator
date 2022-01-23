ca612bec49b867f1e45770a7dc77a1caff49af34
Qhope <vuquanghopvn2001@gmail.com>
2022-01-23 22:02:29 +0700

    Caculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Row, Col} from 'antd'
+import {useState} from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Caculator=()=> {
+    const [value, setValue] = useState('0');
+    const action=(v:string)=>(event:any)=>{
+        event.preventDefault()
+        let preState:string=value
+        if(preState==='0')
+        {
+            preState=''
+        }
+        switch (v) {
+            case'AC':
+                setValue('0')
+                break
+            case"=":
+                try {
+                    setValue(eval(preState).toString())
+                } catch (e) {
+                    setValue('Error')
+                }
+                break
+            default:
+                setValue(preState+v)
+        }
+
+    }
+    return (
+        <div >
+            <Row>
+                <div className="btn screen">{value}</div>
+            </Row>
+    <Row>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('AC')}>AC</Button></Col>
+
+        <Col span={6}><Button className="btn" type="primary" onClick={action('+/-')}>+/-</Button></Col>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('%')}>%</Button></Col>
+
+        <Col span={6}><Button className="btn action" type="primary"onClick={action('/')}>/</Button></Col>
+
+    </Row>
+
+    <Row>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('7')}>7</Button></Col>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('8')}>8</Button></Col>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('9')}>9</Button></Col>
+
+        <Col span={6}><Button className="btn action" type="primary"onClick={action('x')}>x</Button></Col>
+
+    </Row>
+
+    <Row>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('4')}>4</Button></Col>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('5')}>5</Button></Col>
+
+        <Col span={6}><Button  className="btn" type="primary"onClick={action('6')}>6</Button></Col>
+
+        <Col span={6}><Button  className="btn action"type="primary"onClick={action('-')}>-</Button></Col>
+
+    </Row>
+
+    <Row>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('1')}>1</Button></Col>
+
+        <Col span={6}><Button  className="btn"type="primary"onClick={action('2')}>2</Button></Col>
+
+        <Col span={6}><Button className="btn" type="primary"onClick={action('3')}>3</Button></Col>
+
+        <Col span={6}><Button className="btn action" type="primary"onClick={action('+')}>+</Button></Col>
+
+    </Row>
+
+    <Row>
+
+        <Col span={12}><Button className="btn" type="primary"onClick={action('0')}>0</Button></Col>
+
+        <Col span={6}><Button className="btn " type="primary"onClick={action('.')}>.</Button></Col>
+
+        <Col span={6}><Button className="btn action" type="primary"onClick={action('=')}>=</Button></Col>
+
+    </Row>
+
+        </div>
+
+    )
 }
 
-export default App;
+export default Caculator;
